4444764dc55bafe28fe3241a26c60969118dd2bc
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 23:23:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,8 +5,8 @@ def my_collect(array)
   return array
 end
 
-my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |index| index.upcase }
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |index| return index.upcase }
 
 my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) do |index|
-  index.split(' ').first
+  return index.split(' ').first
 end
