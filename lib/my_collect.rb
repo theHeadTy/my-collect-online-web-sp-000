909c7862eab3e945bb572caa5bb886ef702d2ec8
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 23:27:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  new = []
+  array.each do |val|
+    yield val
+    new << yield(val)
+  end
+  new
+end
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |index| puts index.upcase }
 
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) do |index|
+  puts index.split(' ').first
+end
